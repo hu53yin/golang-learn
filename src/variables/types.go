add5241cf291dd8d543a6dcb646f261189b0fa34
Huseyin Gurkan <huseyingurkan@yahoo.com>
2017-12-01 13:16:06 +0300

    Add files via upload

---
+++ src/variables/types.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	x = 42
+	y = "James Bond"
+	z = true
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
+
+//https://play.golang.org/p/uBqKYf5zl7
